4f4a6b5198e66453e57d5bbd6a2a4c96551aeb98
prueba.sonar <prueba.sonar@yopmail.com>
2018-03-02 13:14:56 -0600

    Inicio de proyecto

---
+++ WelcomeProject/Model/Tools.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WelcomeProject.Model
+{
+    public class Tools
+    {
+        public Tools()
+        {
+
+        }
+
+        public void PrintName(string name)
+        {
+            Console.WriteLine("PrintName");
+        }
+    }
+}
